7ed517b7a53b91685bdc274c754a6dfa613a4528
bykimberly <145738163+bykimberly@users.noreply.github.com>
2024-10-09 09:42:45 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import random
 import math
-import pa3
+import affineRSA
 
-print("Welcome to the PA #3 Tester")
+print("-- Welcome to the Encryption/Decryption Tester --")
 
 while True:
   user_in = input(
@@ -14,7 +14,7 @@ while True:
     print("\n" + "-" * 50 + "\n\nTesting Problem 1...\n\n")
 
     expected1 = 'INOTTOZZSNKOJ'
-    received1 = pa3.affine_encrypt('STOP POLLUTION', 5, 22)
+    received1 = affineRSA.affine_encrypt('STOP POLLUTION', 5, 22)
     print(
         f"Testing affine_encrypt(\"STOP POLLUTION\", 5, 22)....\n\tExpected: {expected1} \n\tReceived: {received1}"
     )
@@ -25,7 +25,7 @@ while True:
       print("\tTest FAILED.")
 
     expected2 = 'WGVXFCVO'
-    received2 = pa3.affine_encrypt("BLACK HAT", 1, -31)
+    received2 = affineRSA.affine_encrypt("BLACK HAT", 1, -31)
     print(
         f"\nTesting affine_encrypt(\"BLACK HAT\", 1, -31)....\n\tExpected: {expected2} \n\tReceived: {received2}"
     )
@@ -39,7 +39,7 @@ while True:
         f"\nTesting affine_encrypt(\"SOME MESSAGE\", 2, 3)...\n\tExpected: ValueError"
     )
     try:
-      received = pa3.affine_encrypt("SOME MESSAGE", 2, 3)
+      received = affineRSA.affine_encrypt("SOME MESSAGE", 2, 3)
       print("\tReceived:", received)
       print("\tTest FAILED.")
     except ValueError:
@@ -49,7 +49,7 @@ while True:
     print("\n" + "-" * 50 + "\n\nTesting Problem 2...\n\n")
 
     expected1 = 'STOPPOLLUTION'
-    received1 = pa3.affine_decrypt('INOttOZZSnKOJ', 5, 22)
+    received1 = affineRSA.affine_decrypt('INOttOZZSnKOJ', 5, 22)
     print(
         f"Testing affine_decrypt('INOttOZZSnKOJ', 5, 22)....\n\tExpected: {expected1} \n\tReceived: {received1}"
     )
@@ -60,7 +60,7 @@ while True:
       print("\tTest FAILED.")
 
     expected2 = 'BLACKHAT'
-    received2 = pa3.affine_decrypt('WGVXFCVO', 1, -31)
+    received2 = affineRSA.affine_decrypt('WGVXFCVO', 1, -31)
     print(
         f"\nTesting affine_decrypt('WGVXFCVO', 1, -31)....\n\tExpected: {expected2} \n\tReceived: {received2}"
     )
@@ -76,7 +76,7 @@ while True:
     expected = ['20812182', '09810461', '208121821346', '194319342299']
 
     for i in range(len(texts)):
-      cipher = pa3.encryptRSA(texts[i], 2537, 13)
+      cipher = affineRSA.encryptRSA(texts[i], 2537, 13)
       print(
           f"Testing encryptRSA({texts[i]}, 2537, 13)\n\tReceived: {cipher}\n\tExpected: {expected[i]}"
       )
@@ -91,7 +91,7 @@ while True:
     ciphers = ['20812182', '09810461', '208121821346', '194319342299']
 
     for i in range(len(ciphers)):
-      text = pa3.decryptRSA(ciphers[i], 43, 59, 13)
+      text = affineRSA.decryptRSA(ciphers[i], 43, 59, 13)
       print(
           f"Testing decryptRSA({ciphers[i]}, 43, 59, 13)\n\tReceived: {text}\n\tExpected: {expected[i]}"
       )
